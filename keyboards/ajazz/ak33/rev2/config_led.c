3b6a4fbf77609e92403a38c5dfa2eddcfef21b05
Adam Honse <calcprogrammer1@gmail.com>
2021-03-25 14:18:56 -0500

    [Ajazz AK33 SN32F248B] Enable OpenRGB support

--- keyboards/ajazz/ak33/rev2/config_led.c
+++ keyboards/ajazz/ak33/rev2/config_led.c
@@ -26,4 +26,40 @@ led_config_t g_led_config = { {
    5, 5, 5, 0, 0, 0, 5, 0, 0, 0, 5, 5, 5, 5, 0, 5,
 } };
 
+#ifdef OPENRGB_ENABLE
+const openrgb_config_t g_openrgb_config =
+{
+    // Zones names
+    {"Keyboard"},
+
+    // Zones types
+    {OPENRGB_ZONE_TYPE_MATRIX},
+
+    // Zones sizes
+    {96},
+
+
+#ifdef OPENRGB_USE_CUSTOM_MATRIX_MAP
+    // LED matrix map
+    {
+        {   0, NO_LED,      2,      3,      4,      5,      6,      7,      8,      9,     10,     11,     12,     13,     14,     15  },
+        {  16,     17,     18,     19,     20,     21,     22,     23,     24,     25,     26,     27,     28,     29, NO_LED,     31  },
+        {  32,     33,     34,     35,     36,     37,     38,     39,     40,     41,     42,     43,     44,     45, NO_LED,     47  },
+        {  48,     49,     50,     51,     52,     53,     54,     55,     56,     57,     58,     59, NO_LED,     61, NO_LED,     63  },
+        {  64, NO_LED,     66,     67,     68,     69,     70,     71,     72,     73,     74,     75,     76,     77, NO_LED,     79  },
+        {  80,     81,     82,     NO_LED, NO_LED, NO_LED, 86, NO_LED, NO_LED, NO_LED,     90,     91,     92,     93, NO_LED,     95  }
+    },
+    // Key index to physical position map
+    {
+        {   0, NO_LED,      2,      3,      4,      5,      6,      7,      8,      9,     10,     11,     12,     13,     14,     15  },
+        {  16,     17,     18,     19,     20,     21,     22,     23,     24,     25,     26,     27,     28,     29, NO_LED,     31  },
+        {  32,     33,     34,     35,     36,     37,     38,     39,     40,     41,     42,     43,     44,     45, NO_LED,     47  },
+        {  48,     49,     50,     51,     52,     53,     54,     55,     56,     57,     58,     59, NO_LED,     61, NO_LED,     63  },
+        {  64, NO_LED,     66,     67,     68,     69,     70,     71,     72,     73,     74,     75,     76,     77, NO_LED,     79  },
+        {  80,     81,     82,     NO_LED, NO_LED, NO_LED, 86, NO_LED, NO_LED, NO_LED,     90,     91,     92,     93, NO_LED,     95  }
+    },
+#endif
+};
+#endif
+
 #endif
