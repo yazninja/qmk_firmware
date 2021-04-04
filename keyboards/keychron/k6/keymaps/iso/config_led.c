f03022d8f3ebdc07a526efae678e9615ab34a0a3
stdvar <stdvar@xyz.com>
2021-04-04 00:15:00 -0400

    [Keychron K6] Enable OpenRGB support

--- keyboards/keychron/k6/keymaps/iso/config_led.c
+++ keyboards/keychron/k6/keymaps/iso/config_led.c
@@ -37,4 +37,41 @@ led_config_t g_led_config = { {
    4, 4, 4, 0, 0, 0, 4, 0, 0, 0, 4, 4, 4, 4, 4, 4
 } };
 
+#ifdef OPENRGB_ENABLE
+const openrgb_config_t g_openrgb_config = {
+    // Zones names
+    {"Keyboard"},
+
+    // Zones types
+    {OPENRGB_ZONE_TYPE_MATRIX},
+
+    // Zones sizes
+    {80},
+
+/* This servers as an example - the alt doesn't actually need this, only keyboards where their
+//  g_led_config.matrix_co and layout doesn't actually represnet the true physical form of the keyboard
+//  to get the correct values for the physical_to_hardware_location array
+//  I used the second array on the layout macro at alt.h
+*/
+#    ifdef OPENRGB_USE_CUSTOM_MATRIX_MAP
+    // LED matrix map
+    {
+        {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, NA, 15 },
+        { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, NA, NA, 31 },
+        { 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, NA, 47 },
+        { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, NA, 61, 62, 63 },
+        { 64, 65, 66, NA, NA, NA, 70, NA, NA, NA, 74, 75, 76, 77, 78, 79 }
+    },
+    // Key index to physical position map
+    {
+        {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, NA, 15 },
+        { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, NA, NA, 31 },
+        { 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, NA, 47 },
+        { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, NA, 61, 62, 63 },
+        { 64, 65, 66, NA, NA, NA, 70, NA, NA, NA, 74, 75, 76, 77, 78, 79 }
+    },
+#    endif
+};
+#endif
+
 #endif
