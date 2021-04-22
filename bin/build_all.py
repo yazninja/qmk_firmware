4df5818f8ecaf85c36e8e165e3b184e24f46ab4a
HorrorTroll <sonicvipduc@gmail.com>
2021-04-22 12:38:24 +0700

    Add SPCGear GK530

--- bin/build_all.py
+++ bin/build_all.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-BOARDS = ['redragon/k552/rev1', 'redragon/k552/rev2', 'redragon/k530', 'redragon/k556', 'keychron/k4', 'keychron/k6', 'ajazz/ak33/rev1', 'ajazz/ak33/rev2', 'smartduck/xs61']
+BOARDS = ['redragon/k552/rev1', 'redragon/k552/rev2', 'redragon/k530', 'redragon/k556', 'keychron/k4', 'keychron/k6', 'ajazz/ak33/rev1', 'ajazz/ak33/rev2', 'smartduck/xs61', 'spcgear/gk530']
 
 for kb in BOARDS:
     subprocess.run(f"bin/qmk compile -kb {kb} -km all -j{os.cpu_count()}", shell=True)
