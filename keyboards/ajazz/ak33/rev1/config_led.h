f62c60e25c761e318887746f1255907d1bc53556
Adam Honse <calcprogrammer1@gmail.com>
2021-05-29 00:59:00 -0500

    [Ajazz AK33 Rev1] Condense LED matrix configuration

--- keyboards/ajazz/ak33/rev1/config_led.h
+++ keyboards/ajazz/ak33/rev1/config_led.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LED_MATRIX_COLS 17
 #define LED_MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, A14, A15, B0, B1, B2, B3, B4, B10, B11 }
 
-#define DRIVER_LED_TOTAL (6*17)
+#define DRIVER_LED_TOTAL (82)
 
 #define LED_CAPS_LOCK_PIN D6
 #define LED_PIN_ON_STATE 0
