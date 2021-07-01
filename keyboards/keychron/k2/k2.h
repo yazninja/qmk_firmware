bfb7008725512065bb3de44d0e55b1d73b98a505
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-07-01 14:16:40 +0300

    [Keychron K2] fix compilation

--- keyboards/keychron/k2/k2.h
+++ keyboards/keychron/k2/k2.h
@@ -10,7 +10,7 @@
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d,      k2f, \
     k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b,      k3d,      k3f, \
     k40,      k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b,      k4d, k4e, k4f, \
-    k50, k51, k52,                k56,                k5a, k5b, k5c, k5d, k5e, k5f, \
+    k50, k51, k52,                k56,                k5a, k5b, k5c, k5d, k5e, k5f  \
 ) \
 { \
     {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f}, \
