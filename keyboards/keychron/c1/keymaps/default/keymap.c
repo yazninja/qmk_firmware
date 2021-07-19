1e6adeba2d8c2f0f0b9f6e8cb94cbc76de04b341
vjdato21 <51854537+vjdato21@users.noreply.github.com>
2021-07-19 13:50:01 +0800

    [Keychron C1] Disable VIA, update keymap and readme.md
    Updated 'readme.md' to be appropriate with the keyboard model.

--- keyboards/keychron/c1/keymaps/default/keymap.c
+++ keyboards/keychron/c1/keymaps/default/keymap.c
@@ -21,17 +21,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layer_names {
-    _BASE = 0,
-    _FL,
+    WIN_BASE = 0,
+    WIN_FN = 1,
+    MAC_BASE = 2,
+    MAC_FN = 3,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 /*				+--------------------------------------------------------------------------+----------------+
-				| ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|????|PAUS|
+				| ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|SCRL|PAUS|
 				+--------------------------------------------------------------------------+------|----|----|
- 				|  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |SCRL|PGUP|
+ 				|  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |HOME|PGUP|
  				+--------------------------------------------------------------------------+------|----|----|
  				|  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
  				+--------------------------------------------------------------------------+------|----|----|
@@ -41,43 +43,68 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  				+--------------------------------------------------------------------------+------|----|----|
  				|LCTRL| LGUI| LALT |            SPACE            | RALT| RGUI | FN | RCTRL | |LFT |DWN |RGT |
  				+--------------------------------------------------------------------------+----------------+
+
+				+--------------------------------------------------------------------------+----------------+
+				| ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|SCRL|PAUS|
+				+--------------------------------------------------------------------------+------|----|----|
+ 				|  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |HOME|PGUP|
+ 				+--------------------------------------------------------------------------+------|----|----|
+ 				|  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
+ 				+--------------------------------------------------------------------------+------|----|----|
+ 				| CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN  |                |
+ 				+--------------------------------------------------------------------------+      |----|    |
+ 				| LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT    |      | UP |    |
+ 				+--------------------------------------------------------------------------+------|----|----|
+ 				|LCTRL| LALT| LGUI |            SPACE            | RGUI| RALT | FN | RCTRL | |LFT |DWN |RGT |
+ 				+--------------------------------------------------------------------------+----------------+
 */ 				
     /*  Row:        0          1          2          3        4        5        6         7        8        9          10         11         12         13         14         15         16    */
-    [0] = {     {   KC_ESC,     KC_F1,     KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_PSCR,     TG(2),   RGB_MODE_FORWARD},
-                {   KC_GRV,     KC_1,      KC_2,      KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_INS,    KC_HOME,   KC_PGUP},
-                {   KC_TAB,     KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_DEL,    KC_END,    KC_PGDN},
-                {   KC_CAPS,    KC_A,      KC_S,      KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_NO,     KC_NO},
-                {   KC_LSFT,    KC_NO,     KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_NO,     KC_UP,     KC_NO},
-                {   KC_LCTL,    KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   KC_RGUI,   MO(1),     KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT}
+    [0] = {     {   KC_ESC,     KC_F1,     KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_PSCR,   KC_SLCK,   KC_BRK  },
+                {   KC_GRV,     KC_1,      KC_2,      KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_INS,    KC_HOME,   KC_PGUP },
+                {   KC_TAB,     KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_DEL,    KC_END,    KC_PGDN },
+                {   KC_CAPS,    KC_A,      KC_S,      KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,    KC_NO,     KC_NO,     KC_NO   },
+                {   KC_LSFT,    KC_NO,     KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_NO,     KC_UP,     KC_NO   },
+                {   KC_LCTL,    KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   KC_RGUI,   MO(1),     KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT }
             },
     
-    /*  Row:        0          1          2          3        4        5        6         7        8        9          10         11         12         13         14         15         16        */
-    [1] =   {   {     RESET,  KC_SLCK,  KC_PAUS,   KC_APP,  _______,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC__MUTE,     _______,   _______,    KC_NO,  KC_PSCR,  _______,  RGB_TOG },
-                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,   _______,  _______,  _______,  RGB_M_P,  RGB_HUI },
-                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,   _______,  _______,  _______,  RGB_SAI,  RGB_SPI },
-                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,     KC_NO,  _______,    KC_NO,    KC_NO,    KC_NO },
-                {   _______,    KC_NO,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,     KC_NO,  _______,    KC_NO,  KC_VOLU,    KC_NO },
-                {   _______,  KC_LALT,  KC_LGUI,    KC_NO,    KC_NO,    KC_NO,  _______,    KC_NO,    KC_NO,    KC_NO,   _______,     _______,   _______,  _______,  RGB_MODE_REVERSE,  KC_VOLD,    RGB_MODE_FORWARD }
+    /*  Row:        0              1                   2                   3                   4                   5                   6                   7                   8                   9                   10                  11                  12                  13                  14                  15                  16   */
+    [1] =   {   {   RESET,          KC_BRID,            KC_BRIU,            LGUI(KC_TAB),       LGUI(KC_E),         RGB_VAD,            RGB_VAI,            KC_MPRV,            KC_MPLY,            KC_MNXT,            KC_MUTE,            KC_VOLD,            KC_VOLU,            KC_NO,              LGUI(LSFT(KC_S)),   LGUI(KC_C),         RGB_TOG },
+                {   _______,        _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            RGB_SAI,            RGB_HUI },
+                {   _______,        _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            RGB_SAD,            RGB_HUD },
+                {   _______,        _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            KC_NO,              _______,            KC_NO,              KC_NO,              KC_NO   },
+                {   _______,        KC_NO,              _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            _______,            KC_NO,              _______,            KC_NO,              RGB_SPI,            KC_NO   },
+                {   _______,        KC_LALT,            KC_LGUI,            KC_NO,              KC_NO,              KC_NO,              _______,            KC_NO,              KC_NO,              KC_NO,              _______,            _______,            _______,            _______,            RGB_RMOD,           RGB_SPD,            RGB_MOD }
               },
 
-/*  Row:          0        1        2        3        4        5        6        7        8        9        10       11       12       13       14       15       16    */
-    [2] =   {   {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-                {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-                {  _______,    KC_P,    KC_C,    KC_E, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-                {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-                {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-                {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
-            }
+    /*  Row:        0                   1                   2                   3                   4                   5                   6                   7                   8                   9                   10                  11                  12                  13                  14                  15                  16    */
+    [2] = {     {   KC_ESC,              KC_F1,              KC_F2,              KC_F3,              KC_F4,              KC_F5,              KC_F6,              KC_F7,              KC_F8,              KC_F9,              KC_F10,             KC_F11,             KC_F12,             KC_NO,              LSFT(LGUI(KC_3)),   KC_SLCK,            KC_BRK  },
+                {   KC_GRV,              KC_1,               KC_2,               KC_3,               KC_4,               KC_5,               KC_6,               KC_7,               KC_8,               KC_9,               KC_0,               KC_MINS,            KC_EQL,             KC_BSPC,            KC_INS,             KC_HOME,            KC_PGUP },
+                {   KC_TAB,              KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,               KC_Y,               KC_U,               KC_I,               KC_O,               KC_P,               KC_LBRC,            KC_RBRC,            KC_BSLS,            KC_DEL,             KC_END,             KC_PGDN },
+                {   KC_CAPS,             KC_A,               KC_S,               KC_D,               KC_F,               KC_G,               KC_H,               KC_J,               KC_K,               KC_L,               KC_SCLN,            KC_QUOT,            KC_NO,              KC_ENT,             KC_NO,              KC_NO,              KC_NO   },
+                {   KC_LSFT,             KC_NO,              KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,               KC_N,               KC_M,               KC_COMM,            KC_DOT,             KC_SLSH,            KC_NO,              KC_RSFT,            KC_NO,              KC_UP,              KC_NO   },
+                {   KC_LCTL,             KC_LALT,            KC_LGUI,            KC_NO,              KC_NO,              KC_NO,              KC_SPC,             KC_NO,              KC_NO,              KC_NO,              KC_RGUI,            KC_RALT,            MO(3),              KC_RCTL,            KC_LEFT,            KC_DOWN,            KC_RGHT }
+            },
+
+    /*  Row:        0                     1                     2                     3                     4                     5                     6                     7                     8                     9                     10                    11                    12                    13                    14                    15                    16   */
+    [3] =   {   {   RESET,                 KC_BRID,              KC_BRIU,              _______,              LALT(LGUI(KC_SPC)),   RGB_VAD,              RGB_VAI,              KC_MPRV,              KC_MPLY,              KC_MNXT,              KC_MUTE,              KC_VOLD,              KC_VOLU,              KC_NO,                LSFT(LGUI(KC_4)),     _______,              RGB_TOG },
+                {   _______,               _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              RGB_SAI,              RGB_HUI },
+                {   _______,               _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              RGB_SAD,              RGB_HUD },
+                {   _______,               _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              KC_NO,                _______,              KC_NO,                KC_NO,                KC_NO   },
+                {   _______,               KC_NO,                _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              _______,              KC_NO,                _______,              KC_NO,                RGB_SPI,              KC_NO   },
+                {   _______,               KC_LALT,              KC_LGUI,              KC_NO,                KC_NO,                KC_NO,                _______,              KC_NO,                KC_NO,                KC_NO,                _______,              _______,              _______,              _______,              RGB_RMOD,             RGB_SPD,              RGB_MOD }
+              }
 };
 void dip_switch_update_user(uint8_t index, bool active){
   index = 1;
  if(active){ //Mac mode
 writePin(LED_NUM_LOCK_PIN, 0);
 writePin(LED_SCROLL_LOCK_PIN, 1);
+layer_move(2);
       }
       else{ //Windows mode
 writePin(LED_NUM_LOCK_PIN, 1);
 writePin(LED_SCROLL_LOCK_PIN, 0);
+layer_move(0);
       }
   }
   
--- keyboards/keychron/c1/readme.md
+++ keyboards/keychron/c1/readme.md
@@ -1,12 +1,80 @@
-# Keychron K8
+### Keychron C1
 
-K8 Gateron Hotswap RGB ANSI
+C1 Gateron Hotswap RGB ANSI
 
 Make example for this keyboard (after setting up your build environment):
 
-    make keychron/k8:default
+    make keychron/c1
 
 * * *
-For more info and detailed flashing instructions, please visit https://github.com/alex-dinh/keychron-k8
+# Firmware Details
+
+### Firmware Status:
+* Default - 99% done (Siri Hotkey for MacOS missing)
+* VIA - Currently unsupported.
+
+### Firmware Version:
+* Default - 0.7.101
+* VIA - N/A
+* * *
+# Keymapping
+
+![C1 RGB Layout](https://i.imgur.com/n3JW5Mi.png)
+
+### Windows Mode
+Without Fn | With Fn
+---------- | -------
+F1 | Decrease PC Brightness
+F2 | Increase PC Brightness
+F3 | Task View
+F4 | File Explorer
+F5 | Decrease KB Brightness
+F6 | Increase KB Brightness
+F7 | Previous Track
+F8 | Play/Pause Track
+F9 | Next Track
+F10 | Mute
+F11 | Decrease Volume
+F12 | Increase Volume
+Print Screen | Snip & Sketch
+Scroll lock | Cortana
+Pause/Break | Toggle RGB
+Home | Increase RGB Saturation
+End | Decrease RGB Saturation
+Page Up | Increase RGB Hue
+Page Down | Decrease RGB Hue
+↑ | Increase RGB Effect Speed
+← | Previous RGB Effect
+↓ | Decrease RGB Effect Speed
+→ | Next RGB Effect
+
+### Mac Mode
+Without Fn | With Fn
+---------- | -------
+F1 | Decrease PC Brightness
+F2 | Increase PC Brightness
+F3 | None
+F4 | Finder
+F5 | Decrease KB Brightness
+F6 | Increase KB Brightness
+F7 | Previous Track
+F8 | Play/Pause Track
+F9 | Next Track
+F10 | Mute
+F11 | Decrease Volume
+F12 | Increase Volume
+Take a screenshot (whole screen) | Take a screenshot (specific area)
+Scroll lock | None (Siri's universal hotkey is still unknown)
+Pause/Break | Toggle RGB
+Home | Increase RGB Saturation
+End | Decrease RGB Saturation
+Page Up | Increase RGB Hue
+Page Down | Decrease RGB Hue
+↑ | Increase RGB Effect Speed
+← | Previous RGB Effect
+↓ | Decrease RGB Effect Speed
+→ | Next RGB Effect
+* * *
+For more info and detailed flashing instructions, please visit https://github.com/CanUnesi/QMK-on-K6#readme
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
