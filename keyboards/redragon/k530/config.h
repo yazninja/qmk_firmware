4e440179eee0b8f2a08ec54820f90f51ee1a2ddf
Zsolt Zitting <yellowberry@cocaine.ninja>
2021-08-17 18:14:46 -0600

    [Redragon K530] Add Magic FN feature from stock FW Hold down Caps Lock for 2000ms to enable FN layer until released.

--- keyboards/redragon/k530/config.h
+++ keyboards/redragon/k530/config.h
@@ -39,6 +39,9 @@
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+/* Per-key tapping term, for use with Magic FN */
+#define TAPPING_TERM_PER_KEY
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 //#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
