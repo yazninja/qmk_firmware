73b354d15ed24f5c8d6b20886e4d0c5526536fb5
Adam Honse <calcprogrammer1@gmail.com>
2021-09-18 18:14:33 -0500

    Make RAW_IN/OUT_CAPACITY configurable in chconf.h, set them to 1 for GMMK due to RAM limitations

--- keyboards/glorious/gmmk_full/chconf.h
+++ keyboards/glorious/gmmk_full/chconf.h
@@ -696,6 +696,8 @@
 /*===========================================================================*/
 #define PORT_IDLE_THREAD_STACK_SIZE 0
 #define PORT_INT_REQUIRED_STACK  0
+#define RAW_IN_CAPACITY 1
+#define RAW_OUT_CAPACITY 1
 #endif /* CHCONF_H */
 
 /** @} */
