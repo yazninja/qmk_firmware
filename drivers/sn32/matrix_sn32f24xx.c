8f4279eea7e3735cafaf98ec81c570b9d0720783
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-11-15 23:42:24 +0200

    New Unified Key and RGB Matrix
    This offloads all the PWM payload to Chibios
    Written to be adaptable for sn32*

---
+++ drivers/sn32/matrix_sn32f24xx.c
@@ -0,0 +1,158 @@
+/*
+Copyright 2021 Dimitris Mantzouranis <d3xter93@gmail.com>
+Copyright 2021 Adam Honse <calcprogrammer1@gmail.com>
+Copyright 2011 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Ported to QMK by Stephen Peery <https://github.com/smp4488/>
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ch.h"
+#include "hal.h"
+
+#include "wait.h"
+#include "util.h"
+#include "matrix.h"
+#include "debounce.h"
+#include "quantum.h"
+#include "rgb_matrix_sn32f24xx.h"
+
+static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
+static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
+matrix_row_t raw_matrix[MATRIX_ROWS]; //raw values
+matrix_row_t last_matrix[MATRIX_ROWS] = {0};  // raw values
+matrix_row_t matrix[MATRIX_ROWS]; //debounced values
+
+static bool matrix_changed = false;
+
+__attribute__((weak)) void matrix_init_kb(void) { matrix_init_user(); }
+
+__attribute__((weak)) void matrix_scan_kb(void) { matrix_scan_user(); }
+
+__attribute__((weak)) void matrix_init_user(void) {}
+
+__attribute__((weak)) void matrix_scan_user(void) {}
+
+inline matrix_row_t matrix_get_row(uint8_t row) { return matrix[row]; }
+
+void matrix_print(void) {}
+
+static void init_pins(void) {
+#if(DIODE_DIRECTION == ROW2COL)
+    //  Unselect ROWs
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+        setPinInput(row_pins[x]);
+        writePinHigh(row_pins[x]);
+    }
+#elif(DIODE_DIRECTION == COL2ROW)
+    //  Unselect ROWs
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+        setPinOutput(row_pins[x]);
+        writePinHigh(row_pins[x]);
+    }
+#else
+#error DIODE_DIRECTION must be one of COL2ROW or ROW2COL!
+#endif
+
+    // Unselect COLs
+    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
+        setPinOutput(col_pins[x]);
+        writePinHigh(col_pins[x]);
+    }
+}
+
+void matrix_init(void) {
+    // initialize key pins
+    init_pins();
+
+    // initialize matrix state: all keys off
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        raw_matrix[i] = 0;
+        matrix[i]     = 0;
+    }
+
+    debounce_init(MATRIX_ROWS);
+
+    matrix_init_quantum();
+}
+
+uint8_t matrix_scan(void) {
+    matrix_changed = false;
+    for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++) {
+        for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+            // Determine if the matrix changed state
+            if ((last_matrix[row_index] != raw_matrix[row_index])) {
+                matrix_changed         = true;
+                last_matrix[row_index] = raw_matrix[row_index];
+            }
+        }
+    }
+
+    debounce(raw_matrix, matrix, MATRIX_ROWS, matrix_changed);
+
+    matrix_scan_quantum();
+
+    return matrix_changed;
+}
+
+void matrix_scan_keys(matrix_row_t raw_matrix[], uint8_t current_row){
+        #if(DIODE_DIRECTION == ROW2COL)
+            if(current_row == 0)
+            {   // Read the key matrix
+                for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
+                    // Enable the column
+                    writePinLow(col_pins[col_index]);
+                    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+                        // Check row pin state
+                        if (readPin(row_pins[row_index]) == 0) {
+                            // Pin LO, set col bit
+                            raw_matrix[row_index] |= (MATRIX_ROW_SHIFTER << col_index);
+                        } else {
+                            // Pin HI, clear col bit
+                            raw_matrix[row_index] &= ~(MATRIX_ROW_SHIFTER << col_index);
+                        }
+                    }
+                    // Disable the column
+                    writePinHigh(col_pins[col_index]);
+                }
+            }
+        #elif(DIODE_DIRECTION == COL2ROW)
+            if(current_row == 0)
+            {   // Set all column pins input high
+                for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
+                    setPinInputHigh(col_pins[col_index]);
+                }
+                // Read the key matrix
+                for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+                    // Enable the row
+                    writePinLow(row_pins[row_index]);
+                    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
+                        // Check row pin state
+                        if (readPin(col_pins[col_index]) == 0) {
+                            // Pin LO, set col bit
+                            raw_matrix[row_index] |= (MATRIX_ROW_SHIFTER << col_index);
+                        } else {
+                            // Pin HI, clear col bit
+                            raw_matrix[row_index] &= ~(MATRIX_ROW_SHIFTER << col_index);
+                        }
+                    }
+                    // Disable the row
+                    writePinHigh(row_pins[row_index]);
+                }
+            }
+        #endif
+}
--- drivers/sn32/rgb_matrix_sn32f248b.c
+++ drivers/sn32/rgb_matrix_sn32f248b.c
@@ -1,8 +1,5 @@
-#include <string.h>
-#include "rgb.h"
 #include "rgb_matrix.h"
-#include "rgb_matrix_types.h"
-#include "color.h"
+#include "rgb_matrix_sn32f24xx.h"
 
 /*
     COLS key / led
@@ -36,11 +33,302 @@
     (B)     (E)
     GPIO    GND
 */
-
+static uint16_t row_ofsts[LED_MATRIX_ROWS];
+static uint8_t mr_offset[24] = {0};
+static uint8_t current_row = 0;
 LED_TYPE led_state[LED_MATRIX_ROWS * LED_MATRIX_COLS];
 uint8_t led_pos[DRIVER_LED_TOTAL];
+extern matrix_row_t raw_matrix[MATRIX_ROWS]; //raw values
+static const pin_t led_row_pins[LED_MATRIX_ROWS_HW] = LED_MATRIX_ROW_PINS;
+static const pin_t led_col_pins[LED_MATRIX_COLS] = LED_MATRIX_COL_PINS;
 
-void init(void) {
+/* PWM configuration structure. We use timer CT16B1 with 24 channels. */
+static PWMConfig pwmcfg = {
+    1000000,       /* PWM clock frequency. */
+    256,           /* PWM period (in ticks) 1S (1/10kHz=0.1mS 0.1ms*10000 ticks=1S) */
+    NULL,          /* RGB Callback */
+    {              /* Default all channels to disabled - Channels will be configured durring init */
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0},
+        {PWM_OUTPUT_DISABLED, NULL, 0}
+    },
+    0/* HW dependent part.*/
+};
+
+void rgb_ch_ctrl(PWMConfig *cfg) {
+    
+    /* Enable PWM function, IOs and select the PWM modes for the LED column pins */
+    for(uint8_t i = 0; i < LED_MATRIX_COLS; i++) {
+        switch(led_col_pins[i]) {
+            // Intentional fall-through for the PWM B-pin mapping
+            case B8:
+                cfg->channels[0].pfpamsk = 1;
+            case A0:
+                cfg->channels[0].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[0] = i;
+                break;
+
+            case B9:
+                cfg->channels[1].pfpamsk = 1;
+            case A1:
+                cfg->channels[1].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[1] = i;
+                break;
+            
+            case B10:
+                cfg->channels[2].pfpamsk = 1;
+            case A2:
+                cfg->channels[2].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[2] = i;
+                break;
+
+            case B11:
+                cfg->channels[3].pfpamsk = 1;
+            case A3:
+                cfg->channels[3].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[3] = i;
+                break;
+
+            case B12:
+                cfg->channels[4].pfpamsk = 1;
+            case A4:
+                cfg->channels[4].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[4] = i;
+                break;
+
+            case B13:
+                cfg->channels[5].pfpamsk = 1;
+            case A5:
+                cfg->channels[5].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[5] = i;
+                break;
+
+            case B14:
+                cfg->channels[6].pfpamsk = 1;
+            case A6:
+                cfg->channels[6].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[6] = i;
+                break;
+
+            case B15:
+                cfg->channels[7].pfpamsk = 1;
+            case A7:
+                cfg->channels[7].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[7] = i;
+                break;
+
+            case C0:
+                cfg->channels[8].pfpamsk = 1;
+            case A8:
+                cfg->channels[8].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[8] = i;
+                break;
+
+            case C1:
+                cfg->channels[9].pfpamsk = 1;
+            case A9:
+                cfg->channels[9].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[9] = i;
+                break;
+
+            case C2:
+                cfg->channels[10].pfpamsk = 1;
+            case A10:
+                cfg->channels[10].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[10] = i;
+                break;
+
+            case C3:
+                cfg->channels[11].pfpamsk = 1;
+            case A11:
+                cfg->channels[11].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[11] = i;
+                break;
+
+            case C4:
+                cfg->channels[12].pfpamsk = 1;
+            case A12:
+                cfg->channels[12].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[12] = i;
+                break;
+
+            case C5:
+                cfg->channels[13].pfpamsk = 1;
+            case A13:
+                cfg->channels[13].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[13] = i;
+                break;
+
+            case C6:
+                cfg->channels[14].pfpamsk = 1;
+            case A14:
+                cfg->channels[14].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[14] = i;
+                break;
+
+            case C7:
+                cfg->channels[15].pfpamsk = 1;
+            case A15:
+                cfg->channels[15].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[15] = i;
+                break;
+
+            case C8:
+                cfg->channels[16].pfpamsk = 1;
+            case B0:
+                cfg->channels[16].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[16] = i;
+                break;
+
+            case C9:
+                cfg->channels[17].pfpamsk = 1;
+            case B1:
+                cfg->channels[17].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[17] = i;
+                break;
+
+            case C10:
+                cfg->channels[18].pfpamsk = 1;
+            case B2:
+                cfg->channels[18].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[18] = i;
+                break;
+
+            case C11:
+                cfg->channels[19].pfpamsk = 1;
+            case B3:
+                cfg->channels[19].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[19] = i;
+                break;
+
+            case C12:
+                cfg->channels[20].pfpamsk = 1;
+            case B4:
+                cfg->channels[20].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[20] = i;
+                break;
+
+            case C13:
+                cfg->channels[21].pfpamsk = 1;
+            case B5:
+                cfg->channels[21].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[21] = i;
+                break;
+
+            case C14:
+                cfg->channels[22].pfpamsk = 1;
+            case B6:
+                cfg->channels[22].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[22] = i;
+                break;
+
+            case C15:
+                cfg->channels[23].pfpamsk = 1;
+            case B7:
+                cfg->channels[23].mode = PWM_OUTPUT_ACTIVE_HIGH;
+                mr_offset[23] = i;
+                break;
+        }
+    }
+
+}
+
+void rgb_callback(PWMDriver *pwmp) {
+
+    // Disable the interrupt
+    shared_matrix_rgb_disable();
+    // Turn the selected row off
+    writePinLow(led_row_pins[current_row]);
+
+    // Turn the next row on
+    current_row = (current_row + 1) % LED_MATRIX_ROWS_HW;
+
+    matrix_scan_keys(raw_matrix,current_row);
+
+    uint8_t row_idx = ( current_row / 3 );
+    uint16_t row_ofst = row_ofsts[row_idx];
+    
+    for(uint8_t i=0; i<24; i++){
+        uint8_t state_r = led_state[row_ofst + mr_offset[i] ].r;
+        uint8_t state_b = led_state[row_ofst + mr_offset[i] ].b;
+        uint8_t state_g = led_state[row_ofst + mr_offset[i] ].g;
+        switch(current_row % 3) {
+        case 0:
+            if(state_r >0) {
+                chSysLockFromISR();
+                pwmEnableChannelI(pwmp,i,state_r);
+                chSysUnlockFromISR();
+                break;
+            }
+            goto killit;
+        case 1:
+            if(state_b >0) {
+                chSysLockFromISR();
+                pwmEnableChannelI(pwmp,i,state_b);
+                chSysUnlockFromISR();
+                break;
+            }
+            goto killit;
+        case 2:
+            if(state_g >0) {
+                chSysLockFromISR();
+                pwmEnableChannelI(pwmp,i,state_g);
+                chSysUnlockFromISR();
+                break;
+            }
+            goto killit;
+        default:
+        killit:
+            chSysLockFromISR();
+            pwmDisableChannelI(pwmp,i);
+            chSysUnlockFromISR();
+        }
+    }
+    // Enable the interrupt
+    shared_matrix_rgb_enable();
+    //Restart with new values
+    pwmStart(pwmp, &pwmcfg);
+    writePinHigh(led_row_pins[current_row]);
+}
+
+void shared_matrix_rgb_enable(void) {
+    pwmcfg.callback = rgb_callback;
+    pwmEnablePeriodicNotification(&PWMD1);
+}
+
+void shared_matrix_rgb_disable(void) {
+    // Disable PWM outputs on column pins
+    for(uint8_t i=0;i<24;i++){
+        chSysLockFromISR();
+        pwmDisableChannel(&PWMD1,i);
+        chSysUnlockFromISR();
+    }
+    pwmcfg.callback = NULL;
+    pwmDisablePeriodicNotification(&PWMD1);
+}
+
+void SN32F24XX_init(void) {
     unsigned int i = 0;
     for (unsigned int y = 0; y < LED_MATRIX_ROWS; y++) {
         for (unsigned int x = 0; x < LED_MATRIX_COLS; x++) {
@@ -50,25 +338,36 @@ void init(void) {
             i++;
         }
     }
+    for (uint8_t x = 0; x < LED_MATRIX_ROWS_HW; x++) {
+        setPinOutput(led_row_pins[x]);
+        writePinHigh(led_row_pins[x]);
+    }
+    for (uint8_t i = 0; i < LED_MATRIX_ROWS; i++) {
+        row_ofsts[i] = i * LED_MATRIX_COLS;
+    }
+
+    rgb_ch_ctrl(&pwmcfg);
+    pwmStart(&PWMD1, &pwmcfg);
+    shared_matrix_rgb_enable();
 }
 
 static void flush(void) {}
 
-void set_color(int index, uint8_t r, uint8_t g, uint8_t b) {
+void SN32F24XX_set_color(int index, uint8_t r, uint8_t g, uint8_t b) {
     int corrected_index = led_pos[index];
     led_state[corrected_index].r = r;
     led_state[corrected_index].g = g;
     led_state[corrected_index].b = b;
 }
 
-static void set_color_all(uint8_t r, uint8_t g, uint8_t b) {
+void SN32F24XX_set_color_all(uint8_t r, uint8_t g, uint8_t b) {
     for (int i=0; i<DRIVER_LED_TOTAL; i++)
-        set_color(i, r, g, b);
+        SN32F24XX_set_color(i, r, g, b);
 }
 
 const rgb_matrix_driver_t rgb_matrix_driver = {
-    .init          = init,
+    .init          = SN32F24XX_init,
     .flush         = flush,
-    .set_color     = set_color,
-    .set_color_all = set_color_all,
+    .set_color     = SN32F24XX_set_color,
+    .set_color_all = SN32F24XX_set_color_all,
 };
