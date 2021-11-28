daceccc0a127bd86ac3be9a2b4872c8e6897df07
stdvar <stdvar@xyz.com>
2021-11-28 11:00:29 -0500

    [Keychron K6 rgb/stdvar] Gets os toggle switch to work and update keymap

--- keyboards/keychron/k6/rgb/keymaps/stdvar/keymap.c
+++ keyboards/keychron/k6/rgb/keymaps/stdvar/keymap.c
@@ -60,47 +60,44 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- /*      Row:         0          1          2          3          4         5        6         7        8        9          10         11         12         13               14         15        */
-      [_BASE] = { {   KC_GESC,   KC_1,      KC_2,      KC_3,      KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,         KC_NO,     KC_HOME },
-                  {   KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLASH,       KC_NO,     KC_END  },
-                  {   KC_ENT,    KC_A,      KC_S,      KC_D,      KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,          KC_NO,     KC_INS  },
-                  {   KC_LSFT,   KC_NO,     KC_Z,      TD(X_CTL), KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     OSM(MOD_RSFT),   KC_UP,     KC_DEL  },
-                  {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,     KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RCTRL,  KC_APP,    MO(_FN1),  KC_LEFT,         KC_DOWN,   KC_RGHT }
+ /*      Row:         0                1          2          3          4         5        6         7        8        9          10         11         12         13               14         15        */
+      [_BASE] = { {   KC_GESC,         KC_1,      KC_2,      KC_3,      KC_4,     KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,         KC_NO,     KC_HOME },
+                  {   KC_TAB,          KC_Q,      KC_W,      KC_E,      KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLASH,       KC_NO,     KC_END  },
+                  {   KC_ENT,          KC_A,      KC_S,      KC_D,      KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NO,     KC_ENT,          KC_NO,     KC_INS  },
+                  {   OSM(MOD_LSFT),   KC_NO,     KC_Z,      TD(X_CTL), KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     OSM(MOD_RSFT),   KC_UP,     KC_DEL  },
+                  {   KC_LCTL,         KC_LGUI,   KC_LALT,   KC_NO,     KC_NO,    KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RCTRL,  KC_APP,    MO(_FN1),  KC_LEFT,         KC_DOWN,   KC_RGHT }
                 },
-      [_FN1] = {  {   KC_GESC,   KC_F1,     KC_F2,     KC_F3,     KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,     KC_NO,     KC_PGUP    },
-                  {   KC_NO,     RGB_M_P,   RGB_M_B,   RGB_M_R,   RGB_M_SW, RGB_M_T, RGB_SPD,  RGB_SPI, KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_PGDOWN  },
-                  {   KC_NO,     KC_VOLD,   KC_VOLU,   KC_MPLY,   KC_MUTE,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   MAILFMT,   RGB_SAD,   RGB_SAI,   KC_NO,     KC_NO,     KC_NO,     KC_PGUP    },
-                  {   KC_NO,     KC_NO,     BTLDR,     RGB_TOG,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   RGB_RMOD,  RGB_MOD,   KC_NO,     KC_NO,     KC_NO,     RGB_VAI,   KC_PGDOWN  },
-                  {   KC_GRV,    KC_NO,     KC_NO,     KC_NO,     KC_NO,    KC_NO,   RESET,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     RGB_HUD,   RGB_VAD,   RGB_HUI    }
+      [_FN1] = {  {   KC_GESC,         KC_F1,     KC_F2,     KC_F3,     KC_F4,    KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,           KC_NO,     KC_PGUP    },
+                  {   KC_NO,           RGB_M_P,   RGB_M_B,   RGB_M_R,   RGB_M_SW, RGB_M_T, RGB_SPD,  RGB_SPI, KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,           KC_NO,     KC_PGDOWN  },
+                  {   KC_NO,           KC_VOLD,   KC_VOLU,   KC_MPLY,   KC_MUTE,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   MAILFMT,   RGB_SAD,   RGB_SAI,   KC_NO,     KC_NO,           KC_NO,     KC_PGUP    },
+                  {   KC_NO,           KC_NO,     BTLDR,     RGB_TOG,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   RGB_RMOD,  RGB_MOD,   KC_NO,     KC_NO,     KC_NO,           RGB_VAI,   KC_PGDOWN  },
+                  {   KC_GRV,          KC_NO,     KC_NO,     KC_NO,     KC_NO,    KC_NO,   RESET,    KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     RGB_HUD,         RGB_VAD,   RGB_HUI    }
                }
 };
 
 void dip_switch_update_user(uint8_t index, bool active){
-  switch(index){
-    case 0:
-      if(active){ //BT mode
-// do stuff
-      }
-      else{ //Cable mode
-// do stuff
-      }
-      break;
-    case 1:
-      if(active){ // Win/Android mode
-// do stuff
-      }
-      else{ // Mac/iOS mode
-// do stuff
-      }
-      break;
-  }
+    switch(index){
+        case 0: // macos/windows togggle
+            if(active){ //macos
+                //do something
+                //layer_off(_WINDOWS);
+                //layer_on(_MAC);
+            }
+            else{ //windows
+                //do something
+                //layer_on(_WINDOWS);
+                //layer_off(_MAC);
+            }
+        break;
+    }
 }
+
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  //debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
+    // Customise these values to desired behaviour
+    //debug_enable=true;
+    //debug_matrix=true;
+    //debug_keyboard=true;
+    //debug_mouse=true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
