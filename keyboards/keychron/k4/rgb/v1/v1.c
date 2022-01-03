1d95fdf58cffd5fe235bf98d53aa22720b3d2833
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-03 16:59:40 +0200

    keychron k4: fix json + add numlock indicator in v1

--- keyboards/keychron/k4/rgb/v1/v1.c
+++ keyboards/keychron/k4/rgb/v1/v1.c
@@ -3,4 +3,6 @@
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock)
         RGB_MATRIX_INDICATOR_SET_COLOR(55, 255, 0, 0);
+    if (host_keyboard_led_state().num_lock)
+        RGB_MATRIX_INDICATOR_SET_COLOR(33, 255, 0, 0);
 }
