9e129005cbc18b57165efeadb92a77d464b0583f
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-04 23:21:07 +0200

    sn32: 260: this is already enabled by default

--- platforms/chibios/boards/SN_SN32F260/board/board.mk
+++ platforms/chibios/boards/SN_SN32F260/board/board.mk
@@ -8,7 +8,6 @@ BOARDINC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F260
 # OPT = s /set by default
 
 # Some options to reduce RAM usage
-LDFLAGS += --specs=nano.specs
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 USE_LINK_GC = yes
 LTO_ENABLE = yes
