89ca6c0f9bb6aa52d1d5db939e6eeec6ade5ae05
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-06 16:52:13 +0200

    ffc ffc61: fix build

--- keyboards/ffc/ffc61/rules.mk
+++ keyboards/ffc/ffc61/rules.mk
@@ -1,40 +1,9 @@
 # project specific files
-SRC = ../../drivers/led/sn32/matrix_sn32f24xx.c
+SRC = ../../../drivers/led/sn32/matrix_sn32f24xx.c
 SRC += config_led.c
 
-## chip/board settings
-# - the next two should match the directories in
-#   <chibios>/os/hal/ports/$(MCU_FAMILY)/$(MCU_SERIES)
-MCU_FAMILY = SN32
-MCU_SERIES = SN32F240B
-
-# Linker script to use
-# - it should exist either in <chibios>/os/common/ports/ARMCMx/compilers/GCC/ld/
-#   or <this_dir>/ld/
-MCU_LDSCRIPT = SN32F240B
-
-# Startup code to use
-#  - it should exist in <chibios>/os/common/startup/ARMCMx/compilers/GCC/mk/
-MCU_STARTUP = sn32f24xb
-
-# Board: it should exist either in <chibios>/os/hal/boards/
-#  or <this_dir>/boards
-BOARD = SN_SN32F240B
-
-# Cortex version
-MCU  = cortex-m0
-
-# ARM version, CORTEX-M0/M1 are 6, CORTEX-M3/M4/M7 are 7
-ARMV = 6
-
-# BOOTLOADER = flash
-SN32_BOOTLOADER_ADDRESS = 0x1FFF0301
-
-OPT_DEFS = -O2
-
-# Options to pass to dfu-util when flashing
-# DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
-# DFU_SUFFIX_ARGS = -p DF11 -v 0483
+# MCU name
+MCU = SN32F248BF
 
 # Build Options
 #   comment out to disable the options.
