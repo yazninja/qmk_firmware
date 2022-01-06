954c969a911814929858910a79d10112da8a7be4
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-06 16:47:06 +0200

    flashquark horizon_z: fix build

--- keyboards/flashquark/horizon_z/rules.mk
+++ keyboards/flashquark/horizon_z/rules.mk
@@ -1,6 +1,5 @@
 # project specific files
 SRC = ../../../drivers/led/sn32/matrix_sn32f24xx.c
-SRC += led_matrix.c
 SRC += config_led.c
 
 # MCU name
@@ -29,4 +28,4 @@ DIP_SWITCH_ENABLE = yes
 
 # Custom RGB matrix handling
 RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = custom
+RGB_MATRIX_DRIVER = SN32F24xB
