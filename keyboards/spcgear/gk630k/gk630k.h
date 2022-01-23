e3cb0e88a9ceee1e777e6124dc152ef045b3b1ef
tfuxu <73042332+tfuxu@users.noreply.github.com>
2022-01-23 21:48:50 +0100

    [SPGGear GK630K] Update config.h to reenable some RGB effects, and add support for VIA
    * added support for VIA and its rgb component
    
    * added macro section in gk630k.h file
    
    * updated readme.md to include compiling instructions for gk630k firmware with VIA

--- keyboards/spcgear/gk630k/gk630k.h
+++ keyboards/spcgear/gk630k/gk630k.h
@@ -1,3 +1,23 @@
 #pragma once
 
 #include "quantum.h"
+
+// readability
+#define XXX KC_NO
+
+#define LAYOUT( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,      k0e, k0f, k0g, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f, k1g, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f, k2g, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b,      k3d, k3f, k3e,      \
+    k40,      k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b,      k4d,      k4f,      \
+    k50, k51, k52,                k53,                k54, k55, k56, k58, k5e, k5f, k5g  \
+) \
+{ \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, XXX, k0e, k0f, k0g }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f, k1g }, \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f, k2g }, \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, XXX, k3d, k3e, k3f, XXX }, \
+    { k40, XXX, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b, XXX, k4d, XXX, k4f, XXX }, \
+    { k50, k51, k52, k53, k54, k55, k56, XXX, k58, XXX, XXX, XXX, XXX, XXX, k5e, k5f, k5g }  \
+}
--- keyboards/spcgear/gk630k/readme.md
+++ keyboards/spcgear/gk630k/readme.md
@@ -9,8 +9,14 @@ A compact 87-key (TKL) wired gaming keyboard with RGB lighting.
     * and on Keychron C1 RGB by: [noldevin](https://github.com/noldevin)
 * Hardware Availability: [SPCGear](https://spcgear.com/en/product/gk630k-tournament-kailh-red-rgb/)
 
+## How to compile
+
 Make example for this keyboard (after setting up your build environment):
 
     make spcgear/gk630k:default
+
+Make example with VIA support:
+
+    make spcgear/gk630k:via
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
