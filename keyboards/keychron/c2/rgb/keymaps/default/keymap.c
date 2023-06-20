917a76c2b5c906f7f03ab07fc32399dc6ce9af15
Abraham Lenson <business.lensonabraham@pm.me>
2023-06-20 17:39:36 +0700

    Flip Mac and Windows layer order to correspond with the physical switch (#334)
    * Flip Mac and Windows layer order to correspond with the physical switch
    
    * Also fix MO(layer)
    
    MO(1) > MO(3) on Windows Mode
    MO(3) > MO(1) on Mac Mode

--- keyboards/keychron/c2/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/c2/rgb/keymaps/default/keymap.c
@@ -21,10 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layer_names {
-    WIN_BASE    = 0,
-    WIN_FN      = 1,
-    MAC_BASE    = 2,
-    MAC_FN      = 3,
+    MAC_BASE    = 0,
+    MAC_FN      = 1,
+    WIN_BASE    = 2,
+    WIN_FN      = 3,
 };
 
 #define KC_TASK LGUI(KC_TAB)        // Task viewer
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS },
         {   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   KC_NO,    KC_NO,    KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO   },
         {   KC_LSFT,  KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  KC_NO,    KC_UP,    KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT },
-        {   KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RALT,  KC_RGUI,  MO(1),    KC_NO,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_NO,   KC_P0,   KC_PDOT, KC_NO   }
+        {   KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RALT,  KC_RGUI,  MO(3),    KC_NO,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_NO,   KC_P0,   KC_PDOT, KC_NO   }
     },
 
     [WIN_FN] = { // Windows Fn overlay
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS },
         {   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   KC_NO,    KC_NO,    KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO   },
         {   KC_LSFT,  KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  KC_NO,    KC_UP,    KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT },
-        {   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_NO,    KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RGUI,  KC_RALT,  MO(3),    KC_NO,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_NO,   KC_P0,   KC_PDOT, KC_NO   }
+        {   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_NO,    KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RGUI,  KC_RALT,  MO(1),    KC_NO,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_NO,   KC_P0,   KC_PDOT, KC_NO   }
     },
 
     [MAC_FN] = { // Mac Fn overlay
